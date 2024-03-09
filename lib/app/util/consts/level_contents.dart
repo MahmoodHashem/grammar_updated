86bb4d63c01dfdc68a6cce832b7cc42c30caa3fd
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-03-09 22:38:13 +0430

    Add constants
    In the main page levels have some contents like title, color, remaining lessons, I added them in constants for better modularizing

---
+++ lib/app/util/consts/level_contents.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+
+const pageNo = [0,1,2,3,4,5];
+
+const titles = ['A1', 'A2','B1', 'B2','C1', 'C2'];
+
+const subtitles = ['سطح ابتدائی','سطح ابتدائی','سطح متوسط','سطح متوسط','سطح پیشرفته','سطح پیشرفته'];
+
+const cardColors = [Color(0xFFFF9800), Color(0xFFF57C00),Color(0xFF4CAF50),
+                            Color(0xFF388E3C),Color(0xFFF44336),Color(0xFFD32F2F),];
+
+const subtitleColors = [ Color(0xFFAA3C00), Color(0xFFAA3C00),
+  Color(0xFF0D5212),   Color(0xFF0D5212), Color(0xFF770202), Color(0xFF770202),];
+
+const unSelectedProgressColor = [Color(0xFFFFB74D),Color(0xFFFFB74D),
+  Color(0xFF81C784),Color(0xFF81C784),
+  Color(0xFFE57373),Color(0xFFE57373),];
+
+
+
